b09b1b394021f726f53b95e2ec9a25483944e31f
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-07-14 12:09:36 -0300

    atu

--- script.js
+++ script.js
@@ -20,26 +20,28 @@ setInterval(getCoinValues, 10000); */
 function getCoinValues() {
     // Código para obter os valores das criptomoedas em tempo real
     function getCoinValues() {
-        const coins = ["bitcoin", "ethereum", "litecoin", "binance coin", "xrp"];
+        const coins = ["bitcoin", "ethereum", "litecoin", "binance coin", "xrp", "cardano", "solana", "dogecoin", "polygon", "tron", "polkadot", "axie-infinity"];
         const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coins.join(",")}&vs_currencies=usd`;
     
         fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,litecoin,binancecoin,xrp,cardano,solana,dogecoin,polygon,tron,polkadot,axie-infinity&vs_currencies=usd')
-            .then(response => response.json())
-            .then(data => {
-                document.getElementById("btc-value").textContent = data.bitcoin.usd;
-                document.getElementById("eth-value").textContent = data.ethereum.usd;
-                document.getElementById("ltc-value").textContent = data.litecoin.usd;
-                document.getElementById("bnb-value").textContent = data.binancecoin.usd;
-                document.getElementById("xrp-value").textContent = data.xrp.usd;
-                document.getElementById("ada-value").textContent = data.cardano.usd;
-                document.getElementById("sol-value").textContent = data.solana.usd;
-                document.getElementById("doge-value").textContent = data.dogecoin.usd;
-                document.getElementById("matic-value").textContent = data.polygon.usd;
-                document.getElementById("trx-value").textContent = data.tron.usd;
-                document.getElementById("dot-value").textContent = data.polkadot.usd;
-                document.getElementById("axax-value").textContent = dataaxie-infinity.usd;
-            })
-            .catch(error => console.log(error));
+        .then(response => response.json())
+        .then(data => {
+          document.getElementById("btc-value").textContent = data.bitcoin.usd;
+          document.getElementById("eth-value").textContent = data.ethereum.usd;
+          document.getElementById("ltc-value").textContent = data.litecoin.usd;
+          document.getElementById("bnb-value").textContent = data.binancecoin.usd;
+          document.getElementById("xrp-value").textContent = data.xrp.usd;
+          document.getElementById("ada-value").textContent = data.cardano.usd;
+          document.getElementById("sol-value").textContent = data.solana.usd;
+          document.getElementById("doge-value").textContent = data.dogecoin.usd;
+          document.getElementById("matic-value").textContent = data.polygon.usd;
+          document.getElementById("trx-value").textContent = data.tron.usd;
+          document.getElementById("dot-value").textContent = data.polkadot.usd;
+          document.getElementById("avax-value").textContent = data.axie-infinity.usd;
+        })
+        .catch(error => {
+          console.log('Erro ao obter cotações de criptomoedas:', error);
+        });
     }
     
     window.addEventListener("load", getCoinValues);
