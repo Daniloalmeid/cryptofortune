8c413aa0b1e0100f45196e97b0b9348ba858c4dd
Daniloalmeid <danilotecteatro@yahoo.com.br>
2023-07-14 15:36:18 -0300

    atu

--- script.js
+++ script.js
@@ -1,57 +1,54 @@
- /* crutomoedas*/
-/*
- function getCoinValues() {
-    const coins = ["bitcoin", "ethereum", "litecoin"];
-    const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coins.join(",")}&vs_currencies=usd`;
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("btc-value").textContent = data.bitcoin.usd;
-            document.getElementById("eth-value").textContent = data.ethereum.usd;
-            document.getElementById("ltc-value").textContent = data.litecoin.usd;
-        })
-        .catch(error => console.log(error));
-}
-
-window.addEventListener("load", getCoinValues);
-setInterval(getCoinValues, 10000); */
-
-function getCoinValues() {
-    // Código para obter os valores das criptomoedas em tempo real
-    function getCoinValues() {
-        const coins = ["bitcoin", "ethereum", "litecoin", "binance coin", "xrp", "cardano", "solana", "dogecoin", "polygon", "tron", "polkadot", "axie-infinity"];
-        const url = `https://api.coingecko.com/api/v3/simple/price?ids=${coins.join(",")}&vs_currencies=usd`;
-    
-        fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum,litecoin,binancecoin,xrp,cardano,solana,dogecoin,polygon,tron,polkadot,axie-infinity&vs_currencies=usd')
-        .then(response => response.json())
-        .then(data => {
-          document.getElementById("btc-value").textContent = data.bitcoin.usd;
-          document.getElementById("eth-value").textContent = data.ethereum.usd;
-          document.getElementById("ltc-value").textContent = data.litecoin.usd;
-          document.getElementById("bnb-value").textContent = data.binancecoin.usd;
-          document.getElementById("xrp-value").textContent = data.xrp.usd;
-          document.getElementById("ada-value").textContent = data.cardano.usd;
-          document.getElementById("sol-value").textContent = data.solana.usd;
-          document.getElementById("doge-value").textContent = data.dogecoin.usd;
-          document.getElementById("matic-value").textContent = data.polygon.usd;
-          document.getElementById("trx-value").textContent = data.tron.usd;
-          document.getElementById("dot-value").textContent = data.polkadot.usd;
-          document.getElementById("avax-value").textContent = data.axie-infinity.usd;
-        })
-        .catch(error => {
-          console.log('Erro ao obter cotações de criptomoedas:', error);
-        });
-    }
-    
-    window.addEventListener("load", getCoinValues);
-    setInterval(getCoinValues, 10000); 
-    // Resto do seu código para atualizar os valores das criptomoedas
 
 
-} 
+  $(document).ready(function() {
+    fetch('https://api.coincap.io/v2/assets?limit=20')
+      .then(response => response.json())
+      .then(data => {
+        const cryptoQuotes = data.data;
+  
+        const btcValue = cryptoQuotes.find(crypto => crypto.symbol === 'BTC');
+        document.getElementById('btc-value').textContent = parseFloat(btcValue.priceUsd).toFixed(2);
+  
+        const ethValue = cryptoQuotes.find(crypto => crypto.symbol === 'ETH');
+        document.getElementById('eth-value').textContent = parseFloat(ethValue.priceUsd).toFixed(2);
+  
+        const ltcValue = cryptoQuotes.find(crypto => crypto.symbol === 'LTC');
+        document.getElementById('ltc-value').textContent = parseFloat(ltcValue.priceUsd).toFixed(2);
+  
+        const bnbValue = cryptoQuotes.find(crypto => crypto.symbol === 'BNB');
+        document.getElementById('bnb-value').textContent = parseFloat(bnbValue.priceUsd).toFixed(2);
+  
+        const xrpValue = cryptoQuotes.find(crypto => crypto.symbol === 'XRP');
+        document.getElementById('xrp-value').textContent = parseFloat(xrpValue.priceUsd).toFixed(2);
+  
+        const adaValue = cryptoQuotes.find(crypto => crypto.symbol === 'ADA');
+        document.getElementById('ada-value').textContent = parseFloat(adaValue.priceUsd).toFixed(2);
+  
+        const solValue = cryptoQuotes.find(crypto => crypto.symbol === 'SOL');
+        document.getElementById('sol-value').textContent = parseFloat(solValue.priceUsd).toFixed(2);
+  
+        const dogeValue = cryptoQuotes.find(crypto => crypto.symbol === 'DOGE');
+        document.getElementById('doge-value').textContent = parseFloat(dogeValue.priceUsd).toFixed(2);
+  
+        const maticValue = cryptoQuotes.find(crypto => crypto.symbol === 'MATIC');
+        document.getElementById('matic-value').textContent = parseFloat(maticValue.priceUsd).toFixed(2);
+  
+        const trxValue = cryptoQuotes.find(crypto => crypto.symbol === 'TRX');
+        document.getElementById('trx-value').textContent = parseFloat(trxValue.priceUsd).toFixed(2);
+  
+        const dotValue = cryptoQuotes.find(crypto => crypto.symbol === 'DOT');
+        document.getElementById('dot-value').textContent = parseFloat(dotValue.priceUsd).toFixed(2);
+  
+        const avaxValue = cryptoQuotes.find(crypto => crypto.symbol === 'AVAX');
+        document.getElementById('avax-value').textContent = parseFloat(avaxValue.priceUsd).toFixed(2);
+      })
+      .catch(error => {
+        console.log('Erro ao obter cotações de criptomoedas:', error);
+      });
+  });
 
-
+  
+  
 
 
 window.addEventListener("load", function() {
@@ -67,20 +64,5 @@ window.addEventListener("load", function() {
     getCoinValues();
 });
 
-setInterval(getCoinValues, 10000);
+setInterval(getCoinValues, 10000); 
 
-/*window.addEventListener("load", function() {
-    $('.carousel').slick({
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        speed: 1000
-    });
-
-    getCoinValues();
-});
-
-setInterval(getCoinValues, 10000); */
-  
